1cc66a51dfa6fa37e4bba2b5177e8047eb9001af
Amit399 <71559190+Amit399@users.noreply.github.com>
2022-07-11 00:18:22 +0530

    Update linkStack.cpp

--- Stack/linkStack.cpp
+++ Stack/linkStack.cpp
@@ -1,62 +1,85 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-//complete the functions
+struct Node
+{
+    int value;
+    Node* next;
+};
+ 
 class Stack{
-	int value,n ;
-	int *start;
-	int *next;
-	
+	int n=0;
+	Node* start;
+	Node* next;
+ 
 	public:
 	//constructor is called whenever an object of the class is declared
 	Stack(){
 		cout<<"constructor is Called\n";
 		start=NULL;
-		n=0;
 	}
-	
+ 
 	//push the value at the back
 	void push(int val){
-		cout<<"pushing the val: "<<val<<" in Stack\n";
-		Stack* temp= new Stack;
+		cout<<"pushing the val: "<<val<<" at the back of queue\n";
+		Node* temp= new Node;
 		temp->value=val;
-		temp->next=start;
-		start=temp;
+		if(isEmpty())
+		{
+		    temp->next=NULL;
+		    start=temp;
+		}
+		else
+		{
+		    temp->next=start;
+		    start=temp;
+		}
 		n++;
+		/*if(isEmpty())
+		{
+    		start=end=temp;
+    		n++;
+		}
+		else
+		{
+		    end->next=temp;
+		    end=temp;
+		    n++;
+		}*/
+		
 	}
-	
+ 
 	int front(){
 		cout<<"returning the value at front\n";
 		return start->value;
 	}
-	
+ 
 	void pop(){
 		cout<<"removing the first element\n";
 		if(isEmpty())
 		{
 		    return;
 		}
+		Node* temp;
+		temp=start;
 		start=start->next;
 		n--;
 	}
-	
+ 
 	int size(){
 		cout<<"return the size of the queue\n";
 		return n;
 	}
-	
+ 
 	bool isEmpty(){
 		cout<<"returning if the queue is empty\n";
-		if(start==NULL)
+		if(!start)
 		{
 		    return true;
 		}
-		else{
-		    return false;
-		}
+		return false;
 	}
 };
-
+ 
 int main() {
 	Stack q; //constructor is called on this line
 	q.push(4);
@@ -64,14 +87,14 @@ int main() {
 	q.pop();
 	cout<<"size: "<<q.size()<<"\n";
 	cout<<"empty: "<<q.isEmpty()<<"\n";
-	
+ 
 	q.push(5);
 	q.push(3);
 	cout<<"front: "<<q.front()<<"\n";
 	q.pop();
 	cout<<"size: "<<q.size()<<"\n";
 	cout<<"empty: "<<q.isEmpty()<<"\n";
-	
+ 
 	cout<<"front: "<<q.front()<<"\n";
 	q.pop();
 	cout<<"size: "<<q.size()<<"\n";
